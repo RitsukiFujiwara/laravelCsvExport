e922de8b92e2c8838501ac2e0a3e4271e82289e1
Fujiwara <r9.20important@icloud.com>
2021-06-25 18:06:32 +0900

    完成

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Contact;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ContactController extends Controller
 {
@@ -18,5 +19,34 @@ class ContactController extends Controller
             ->get();
 
         return view('index', compact('contacts'));
+    }
+
+    public function csvExport(Request $request)
+    {
+        $post = $request->all();
+        $response = new StreamedResponse(function () use ($request, $post) {
+            $stream = fopen('php://output', 'w');
+            $contact = new Contact();
+            stream_filter_prepend($stream, 'convert.iconv.utf-8/cp932/TRANSLIT');
+            // ヘッダー行を追加
+            fputcsv($stream, $contact->csvHeader());
+
+            $results = $contact->getCsvData($post['start_date'], $post['end_date']);
+
+            if (empty($results[0])) {
+                fputcsv($stream, [
+                    'データが存在しませんでした。',
+                ]);
+            } else {
+                foreach ($results as $row) {
+                    fputcsv($stream, $contact->csvRow($row));
+                }
+            }
+            fclose($stream);
+        });
+        $response->headers->set('Content-Type', 'application/octet-stream');
+        $response->headers->set('content-disposition', 'attachment; filename=' . $post['start_date'] . '〜' . $post['end_date'] . 'お問い合わせ一覧.csv');
+
+        return $response;
     }
 }
